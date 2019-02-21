407b2df0437df9fe9bb5f126a870d8285017c227
Sithiz <Joshklaas1@gmail.com>
2019-02-21 14:40:54 -0600

    Added some Router Functionality'

---
+++ roadapi/src/Body/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+
+export default class About extends Component{
+
+
+	render(){
+		return( 
+			<div>
+				<h1>About the Project</h1>
+				<p>I would like to build out a an app to help you select a netflix movie to watch for a road trip. The website will show you a photo of of the series and offer a description and series length. It will incorpate the Backend netflix database I have alraedy created to serve the website information. I plan to pull data in from google maps api to let you compare time of trip to series length ultimatly helping you decided which series to watch.
+				</p>
+			</div>
+
+			)
+	}
+}
